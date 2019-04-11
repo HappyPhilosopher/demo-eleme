38e9bb2c59bddfcaa0baeae840acd224fdc473fa
Yang <576772193@qq.com>
2019-04-12 00:29:41 +0800

    header-detail开发

--- sell/src/components/goods/Goods.vue
+++ sell/src/components/goods/Goods.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="header">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum vero eligendi veniam molestiae aspernatur molestias maiores facilis, nulla cumque iusto tempora beatae sequi! Sunt corporis exercitationem totam impedit quaerat maiores iste hic animi? Suscipit error culpa itaque obcaecati mollitia in rem repudiandae esse dolor nesciunt! Dolorum explicabo, omnis possimus illo commodi aspernatur eos nemo, neque id ipsum aperiam, dicta minus reprehenderit. Accusantium ipsum amet optio maxime, blanditiis, nulla dolores eum odio error a nemo culpa sunt maiores voluptates voluptas molestiae adipisci tempore. Repudiandae officiis, eveniet quos debitis ipsam distinctio vitae, sit eum, dolorem doloremque ea et dolorum reiciendis voluptatibus rem!
+        Goods
     </div>
 </template>
 
--- sell/src/components/header/Header.vue
+++ sell/src/components/header/Header.vue
@@ -18,14 +18,27 @@
         </div>
         <div class="supports-count">
           <span v-if="seller.supports" class="count">{{ seller.supports.length }}个</span>
-          <span class="icon-keyboard_arrow_right"></span>
+          <span class="icon-keyboard_arrow_right" @click="showDetails()"></span>
         </div>
       </div>
     </div>
     <div class="bulletin-wrapper">
       <span class="bulletin-title"></span>
       <span class="bulletin-text">{{ seller.bulletin }}</span>
-      <i class="icon-keyboard_arrow_right"></i>
+      <i class="icon-keyboard_arrow_right" @click="showDetails()"></i>
+    </div>
+    <div class="header-background">
+      <img :src="seller.avatar" alt="背景图" width="100%" height="100%">
+    </div>
+    <div class="detail" v-show="detailShow">
+      <div class="detail-wrapper">
+        <div class="detail-main">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam quos cum itaque blanditiis rem fugit, voluptates sunt praesentium quae a, deserunt nostrum. Officiis unde labore perferendis vitae! Animi, et. Blanditiis quia quaerat hic quasi totam, optio dicta temporibus delectus quam? Quis molestiae, suscipit doloribus ea, ad inventore possimus non culpa ratione id mollitia vel? Rerum qui velit officia. Fuga et, sapiente quam eos quibusdam quas sit inventore voluptas officia unde aliquam amet reprehenderit. Architecto aut obcaecati sit ut necessitatibus nisi quasi illum porro odio consequatur, quos, ullam, omnis vero autem ratione ipsa natus quia possimus quaerat voluptate esse quod enim! Labore accusamus dolorem qui alias dignissimos recusandae aspernatur nobis suscipit, facere, cupiditate adipisci laudantium amet libero! Ipsum aliq
+        </div>
+      </div>
+      <div class="detail-close" @click="hideDetails()">
+        <i class="icon-close"></i>
+      </div>
     </div>
   </div>
 </template>
@@ -35,10 +48,18 @@ export default {
   props: ["seller"],
   data() {
     return {
-      supportsIconClassList: []
+      supportsIconClassList: [],
+      detailShow: false
     };
   },
-  methods: {},
+  methods: {
+    showDetails() {
+      this.detailShow = true;
+    },
+    hideDetails() {
+      this.detailShow = false;
+    }
+  },
   created() {
     this.supportsIconClassList = ['decrease', 'discount', 'guarantee', 'invoice', 'special'];
   }
@@ -50,7 +71,9 @@ export default {
 @import '../../common/styles/style.css';
 
 .header {
+  position: relative;
   background-color: rgba(7, 17, 27, 0.5);
+  overflow: hidden;
 
   .content-wrapper {
     position: relative;
@@ -147,12 +170,13 @@ export default {
   }
 
   .bulletin-wrapper {
+    position: relative;
     width: 100%;
     height: 0.56rem;
     background-color: rgba(7, 17, 27, 0.2);
-    padding: 0 0.24rem;
+    padding: 0 0.44rem 0 0.24rem;
     font-size: 0.2rem;
-    line-height: 0.56;
+    line-height: 0.56rem;
     text-overflow: ellipsis;
     white-space: nowrap;
     color: #fff;
@@ -164,11 +188,63 @@ export default {
       width: 0.44rem;
       height: 0.24rem;
       @include img-dpr('./images/bulletin');
+      vertical-align: top;
+      margin-top: 0.16rem;
     }
 
     .bulletin-text {
       margin-left: 0.08rem;
       margin-right: 0.08rem;
+    }
+
+    .icon-keyboard_arrow_right {
+      position: absolute;
+      top: 50%;
+      right: 0.24rem;
+      transform: translateY(-50%);
+      font-size: 0.2rem;
+    }
+  }
+
+  .header-background {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    filter: blur(10px);
+    z-index: -1;
+  }
+
+  .detail {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    min-height: 100%;
+    display: flex;
+    flex-direction: column;
+    background-color: rgba(0, 0, 0, 0.8);
+    z-index: 100;
+
+    .detail-wrapper {
+      flex: 1;
+      width: 100%;
+      color: #fff;
+      box-sizing: border-box;
+
+      .detail-main {
+        margin-top: 1.28rem;
+      }
+    }
+
+    .detail-close {
+      width: 100%;
+      height: 1.28rem;
+      font-size: 0.64rem;
+      color: rgba(255, 255, 255, 0.5);
+      text-align: center;
     }
   }
 }
